3a9122ad899723c58cee12e3f1fbd408938d40ff
M <vignesh.g.m@capgemini.com>
2023-05-25 13:05:40 +0530

    comments by viggy

---
+++ src/test/java/com/cap/testrunner99/TestRunner.java
@@ -0,0 +1,20 @@
+package com.cap.testrunner99;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		junit= "--step-notifications",
+		plugin = {"json:target/cucumber.json"},
+		features = "Feature"
+		,glue={"step_definition"}
+		//,tags={"@first_name_field_validation,@succesful_login,@last_name_validation"}//or 
+		//,tags= {"@HotelBooking"}
+		,tags= {"@incorrect_mobileNo_format"}
+		)
+public class TestRunner {
+
+}
